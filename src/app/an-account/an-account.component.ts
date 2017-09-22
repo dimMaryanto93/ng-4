a628611f8792b33a6a258a43ee42c0a9168b4877
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-23 01:33:34 +0700

    create component view

---
+++ src/app/an-account/an-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-an-account',
+  templateUrl: './an-account.component.html',
+  styleUrls: ['./an-account.component.css']
+})
+export class AnAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
