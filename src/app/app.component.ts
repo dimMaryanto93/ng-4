9213699aaadfe3449c626dc9e8eec493ff1d9641
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-23 10:37:42 +0700

    create a view with single component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  serverName: string;
+  serverContent: string;
+
+  serverElements: { name: string, content: string, type: string }[] = [
+    {name: 'Test Server', content: 'Some content', type: 'blueprint'},
+    {name: 'Production server', content: 'some content', type: 'server'},
+    {name: 'Development server', content: 'some text', type: 'server'}
+  ];
+
+
+  addNewServer() {
+    this.serverElements.push({name: this.serverName, content: this.serverContent, type: 'server'});
+    this.serverName = null;
+    this.serverContent = null;
+  }
+
+  addNewServerBlueprint() {
+    this.serverElements.push({name: this.serverName, content: this.serverContent, type: 'blueprint'});
+    this.serverName = null;
+    this.serverContent = null;
+  }
 }
