4f709592f9de8e512395d31e9dc665f82a34a1d4
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-27 00:11:52 +0700

    using @ViewChild to access element html

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 
 @Component({
@@ -8,22 +8,23 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  @ViewChild('serverName') serverName: ElementRef;
   @Output() serverCreated = new EventEmitter<{ nama: string, content: string }>();
   @Output('serverBlueprintCreated') blueprintCreated = new EventEmitter<{ nama: string, content: string }>();
 
-  serverName: string;
+  // serverName: string;
   serverContent: string;
 
   addNewServer() {
     // this.serverElements.push({name: this.nama, content: this.content, type: 'server'});
-    this.serverCreated.emit({nama: this.serverName, content: this.serverContent});
+    this.serverCreated.emit({nama: this.serverName.nativeElement.value, content: this.serverContent});
     this.serverName = null;
     this.serverContent = null;
   }
 
   addNewServerBlueprint() {
     // this.serverElements.push({name: this.nama, content: this.content, type: 'blueprint'});
-    this.blueprintCreated.emit({nama: this.serverName, content: this.serverContent});
+    this.blueprintCreated.emit({nama: this.serverName.nativeElement.value, content: this.serverContent});
     this.serverName = null;
     this.serverContent = null;
   }
