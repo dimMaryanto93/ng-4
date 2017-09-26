a6a9b3a6e54b250b5010b4ea0dbf3b8c1d1e9a04
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-27 00:43:40 +0700

    connection FormControl in typescript into template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import {Component} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders: string[] = ['Male', 'Female'];
   submitForm: FormGroup;
+
+  ngOnInit() {
+    this.submitForm = new FormGroup({
+      'username': new FormControl(),
+      'email': new FormControl(null),
+      'gender': new FormControl('Female')
+    });
+  }
 }
