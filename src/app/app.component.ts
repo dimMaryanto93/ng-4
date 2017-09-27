fdfbb826bec9b7777e9c72d3457f0edf7aece68e
dimMaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-09-27 10:33:18 +0700

    using pipes to transform output data from component data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.servers.push({
+      name: 'Production Server', scale: 'medium', startedDate: new Date()
+    });
+
+    this.servers.push({
+      name: 'Development Server', scale: 'low', startedDate: new Date()
+    })
+  }
+
+  servers: { name: string, scale: string, startedDate: Date }[] = [];
 }
